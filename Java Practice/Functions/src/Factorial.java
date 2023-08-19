74b448c26dd9f8764a6f2de209c950e9a4538de9
Prince Saini <princesaini0144@gmail.com>
2023-08-19 22:20:47 +0530

    add factorial file to function file

---
+++ Java Practice/Functions/src/Factorial.java
@@ -0,0 +1,22 @@
+//Write a program to print the factorial of a number by defining a method named 'Factorial'.
+import java.util.*;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num ;
+        System.out.println("Enter a number : ");
+        num = sc.nextInt();
+
+        int ans = fact(num);
+        System.out.println("Factorial of " + num + " is : " +ans);
+
+    }
+    static int fact(int num){
+        int ans =1;
+        for(int i=num ; i>0 ; i--){
+            ans = ans * i;
+        }
+        return ans;
+
+    }
+}
